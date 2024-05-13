b2b1bec87330e2000c80507fea04072c876be036
Nathalia6508 <102549857+Nathalia6508@users.noreply.github.com>
2024-05-12 20:23:22 -0400

    Add files via upload

---
+++ AnalisadorLexico/src/App.java
@@ -0,0 +1,13 @@
+//import java.util.Scanner;
+
+import ManipuladorArquivos.Carregador;
+// import ManipuladorArquivos.ManipuladorArquivo;
+// import Tokens.Simbolo;
+// import Tokens.TabelaSimbolos;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+		  //ManipuladorArquivo.Leitor("CalculadoraPascal.pas");
+      Carregador.Executar();
+    }
+}
